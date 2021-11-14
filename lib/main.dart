8495bbf527b67b3dd77ea04558521fff89c0ff0f
Eddie Ogbemudia <donfiazo@gmail.com>
2021-11-14 18:39:20 +0100

    Save first flutter

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Homepage(),
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+    )
+  ));
+}
+class Homepage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Eddie Flutter App"),
+      ),
+      body: Container(
+        child: Text ("Hi Mary"),
+        ),
+    );
+  }
+}
+
